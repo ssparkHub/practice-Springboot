f40726520926ca552b7bf8111b4f9df29c0f9d30
sspark <tjdtnf110@naver.com>
2023-04-08 12:17:27 +0900

    #2 AppInit.V3 - WebApplicationInitializer 를 구현하면 발생하는 META-INF, jakarta.servlet.ServletContainerInitializer 를 따로 작성하지 않아도 되는 편리성 확인 - 톰캣을 따로 설치하여 war를 배포 한 뒤 사용하는 방식의 구현.

--- src/main/java/hello/servlet/HelloServlet.java
+++ src/main/java/hello/servlet/HelloServlet.java
@@ -6,12 +6,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.security.Principal;
 
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
-        System.out.println("Hello servlet!");
+        resp.getWriter().println("Hello Servlet!");
+
+        boolean authenticate = req.authenticate(resp);
+        String authType = req.getAuthType();
+        Principal userPrincipal = req.getUserPrincipal();
+
+        resp.getWriter().println("authenticate : " + authenticate);
+        resp.getWriter().println("authType : " + authType);
+        resp.getWriter().println("userPrincipal : " + userPrincipal);
+
     }
 }
